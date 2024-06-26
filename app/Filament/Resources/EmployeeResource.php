2145764521866081c3c1c4df42fa9d48d371fbd7
Pid24 <rofidnasifannafie@gmail.com>
2024-06-26 14:47:35 +0700

    dependant-select-options

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -2,16 +2,21 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\EmployeeResource\Pages;
-use App\Filament\Resources\EmployeeResource\RelationManagers;
-use App\Models\Employee;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
+use App\Models\City;
 use Filament\Tables;
+use App\Models\State;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use App\Models\Employee;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Filament\Resources\Resource;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
+use App\Filament\Resources\EmployeeResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\EmployeeResource\RelationManagers;
 
 class EmployeeResource extends Resource
 {
@@ -22,26 +27,36 @@ class EmployeeResource extends Resource
     protected static ?string $navigationGroup = 'Employee Management';
 
     public static function form(Form $form): Form
-    {
-        return $form
-            ->schema([
-                Forms\Components\Section::make('Relationships')
-                    ->schema([
-                        Forms\Components\Select::make('country_id')
-                            ->relationship(name: 'country', titleAttribute: 'name')
-                            ->searchable()
-                            ->preload()
-                            ->required(),
-                        Forms\Components\Select::make('state_id')
-                            ->relationship(name: 'state', titleAttribute: 'name')
-                            ->searchable()
-                            ->preload()
-                            ->required(),
-                        Forms\Components\Select::make('city_id')
-                            ->relationship(name: 'city', titleAttribute: 'name')
-                            ->searchable()
-                            ->preload()
-                            ->required(),
+{
+    return $form
+        ->schema([
+            Forms\Components\Section::make('Relationships')
+                ->schema([
+                    Forms\Components\Select::make('country_id')
+                        ->relationship(name: 'country', titleAttribute: 'name')
+                        ->searchable()
+                        ->preload()
+                        ->live()
+                        ->afterStateUpdated(fn (Set $set) => $set('state_id', null))
+                        ->required(),
+                    Forms\Components\Select::make('state_id')
+                        ->options(fn (Get $get): array =>
+                            State::where('country_id', $get('country_id'))
+                                ->pluck('name', 'id')
+                                ->all())
+                        ->searchable()
+                        ->preload()
+                        ->live()
+                        ->afterStateUpdated(fn (Set $set) => $set('city_id', null))
+                        ->required(),
+                    Forms\Components\Select::make('city_id')
+                        ->options(fn (Get $get): array =>
+                            City::where('state_id', $get('state_id'))
+                                ->pluck('name', 'id')
+                                ->all())
+                        ->searchable()
+                        ->preload()
+                        ->required(),
                         Forms\Components\Select::make('department_id')
                             ->relationship(name: 'department', titleAttribute: 'name')
                             ->searchable()
